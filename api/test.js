251521e98c0459648be5dbb8686b7b00439797c5
Hanlin Forum <hanlin-forum@users.noreply.github.com>
2026-07-16 11:03:47 +0800

    fix: use X-Auth-Token header for IGA Pages compatibility, update cache to v9

---
+++ api/test.js
@@ -0,0 +1,5 @@
+﻿export default async function handler(request) {
+  return new Response(JSON.stringify({ ok: true, message: "API works!" }), {
+    headers: { "Content-Type": "application/json" }
+  });
+}
